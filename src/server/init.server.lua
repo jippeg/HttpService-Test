68a2779abe26583913cca744a50ab746e1555272
jippeg <jippe.gijsens@student.howest.be>
2023-05-15 19:59:10 +0200

    second

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,12 @@
 local HttpService = game:GetService("HttpService")
+local URL = "https://raw.githubusercontent.com/jippeg/HttpService-Test/master/external/test.json"
+
+local response = HttpService:GetAsync(URL)
+
+local data = HttpService:JSONDecode(response)
+
+if data.message == "success" then
+    for i, v in pairs(data.data) do
+        print(i, v)
+    end
+end
